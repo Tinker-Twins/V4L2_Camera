0cc88aad02eafd9ce512419b0c691f63612f3fed
Tinker Twins <tinkertwinsofficial@gmail.com>
2023-03-09 00:54:53 -0500

    Update camera_view.launch.py

--- ros2_v4l2_camera/launch/camera_view.launch.py
+++ ros2_v4l2_camera/launch/camera_view.launch.py
@@ -24,6 +24,7 @@
 
 from launch import LaunchDescription
 from launch_ros.actions import Node
+from launch.actions import ExecuteProcess
 
 def generate_launch_description():
     return LaunchDescription([
